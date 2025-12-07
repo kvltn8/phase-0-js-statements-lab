d02bf18b6c302afa72e47dd8ec02e1cd8d2ab12c
Kaltun <kaltunabdullahi070@gmail.com>
2025-12-07 10:01:31 +0300

    completed lab gardenManagement.js

--- gardenManagement.js
+++ gardenManagement.js
@@ -1,3 +1,25 @@
 const temperature = 90;
 const timeOfDay = "morning";
 let soilMoisture = 30;
+
+//watering control 
+if(temperature > 80){
+  console.log("Watering on");
+}else{
+  console.log("Watering off");
+}
+
+//garden lights control
+if(timeOfDay === "evening" || timeOfDay === "night"){
+  console.log("Lights on");
+}else{
+  console.log("Lights off")
+}
+
+//soil moisture adjustment
+while(soilMoisture < 40){
+  console.log(soilMoisture);
+  soilMoisture +=5;
+}
+
+console.log(40);
